45c17fcee03424d453074175c981033973e60387
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2020-12-05 11:20:00 +0200

    Update l5.c

--- l5.c
+++ l5.c
@@ -1,33 +1,44 @@
-#include <fcntl.h>
-    #include <sys/stat.h>
-    #include <semaphore.h>
-    #include <stdio.h>
-    #include <string.h> 
-
-    #define SEMAPHORE_NAME "/my_named_semaphore"
-    #define NAMEDPIPE_NAME "/tmp/my_named_pipe"
-    #define BUFSIZE 50
-
-    int main(int argc, char ** argv) {
-    sem_t *sem;
-    int  len;
-    char buf[BUFSIZE];
-      if ( mkfifo(NAMEDPIPE_NAME, 0777) ) {
-       perror("mkfifo");
-      return 1;
-      }
-    printf("%s is created\n", NAMEDPIPE_NAME);
-    if ( (sem = open(NAMEDPIPE_NAME, O_RDONLY)) <= 0 ) {
-    perror("open");
-    return 1;
+   #include <fcntl.h>
+    #include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int
+main(int argc, char *argv[])
+{
+    int pipefd[2];
+    pid_t cpid;
+    char buf;
+    if (argc != 2) {
+        fprintf(stderr, "Использование: %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    printf("%s is opened\n", NAMEDPIPE_NAME);
-
-   
-
-   
-   
-    
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    cpid = fork();
+     switch(cpid) {
+    case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+    case 0:   
+        close(pipefd[1]);          
+        while (read(pipefd[0], &buf, 1) > 0)
+            write(STDOUT_FILENO, &buf, 1);
+        write(STDOUT_FILENO, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+     default:          
+        close(pipefd[0]);        
+        write(pipefd[1], argv[1], strlen(argv[1]));
+        close(pipefd[1]);         
+        wait(NULL);                
+        exit(EXIT_SUCCESS);
+    }
 }
-    
+
+   
     
