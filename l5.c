a7d66d17e807f0b7ce6c0a0eab51e209b9e58524
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2020-12-05 12:13:07 +0200

    Update l5.c

--- l5.c
+++ l5.c
@@ -5,8 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int pipefd[2];
     pid_t cpid;
@@ -26,8 +25,10 @@ main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     case 0:   
         close(pipefd[1]);          
-        while (read(pipefd[0], &buf, 1) > 0)
+        while (read(pipefd[0], &buf, 1) > 0){
+        char ss=toupper(buf);
             write(STDOUT_FILENO, &buf, 1);
+            }
         write(STDOUT_FILENO, "\n", 1);
         close(pipefd[0]);
         _exit(EXIT_SUCCESS);
@@ -41,4 +42,8 @@ main(int argc, char *argv[])
 }
 
    
+   
+    
+
+    
     
